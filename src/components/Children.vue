1fcdd3c0e459d4d87808f79d6883d09f043f56c4
gaocaiyun <gaocaiyun@ccbscf.com>
2020-09-23 17:29:47 +0800

    feat(vue-ts): add slot props

---
+++ src/components/Children.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>
+      <slot></slot>
+    </div>
+    <div>
+      <slot name="title" />
+    </div>
+    <div>
+      <slot name="item" v-bind="{value: 'test', key: 'key'}"/>
+    </div>
+  </div>
+</template>
