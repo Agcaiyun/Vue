69336bafc1b0c9e8916c23405fa02e0ea7c82e62
agcaiyun <agcaiyun@163.com>
2021-09-28 14:26:52 +0800

    feat: vuex学习准备

---
+++ src/components/Bar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Bar</div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../components/Home'
+import Foo from '../components/Foo'
+import Bar from '../components/Bar'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    { path: '/', name: 'home', component: Home},
+    { path: '/foo', name: 'foo', component: Foo},
+    { path: 'bar', name: 'bar', component: Bar}
+  ]
+})
