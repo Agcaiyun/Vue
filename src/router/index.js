f414def78bffc66fe32ee242f496e41168680ad4
Agcaiyun <Agcaiyun@163.com>
2021-09-28 14:59:20 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,6 @@ export default new Router({
   routes: [
     { path: '/', name: 'home', component: Home},
     { path: '/foo', name: 'foo', component: Foo},
-    // { path: '/bar', name: 'bar', component: Bar}
+    { path: '/bar', name: 'bar', component: Bar}
   ]
 })
